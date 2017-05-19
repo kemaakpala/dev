57be1e21eca1080de4dbf77f084d4e339510c32a
Patrick Akpala <kemaakpala@gmail.com>
2017-05-19 19:41:12 +0100

    latest commit

--- mywebsite/public/main.js
+++ mywebsite/public/main.js
@@ -1,2 +1,8 @@
 //Module
-var mywebsiteApp = angular.module('mywebsiteApp', []);
+var mywebsiteApp = angular.module('mywebsiteApp', ['ngRoute', 'ngResource']);
+
+//CONTROLLERS
+
+mywebsiteApp.controller('homeController', ['$scope', function($scope){
+
+}]);
