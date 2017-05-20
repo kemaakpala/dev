7d8dcdf671606fcde746005f1c60835317240d31
patrick akpala <patrick.a@thoughtbubble.com>
2017-05-20 19:04:15 +0100

    laptop changes

--- mywebsite/public/main.js
+++ mywebsite/public/main.js
@@ -1,2 +1,24 @@
 //Module
-var mywebsiteApp = angular.module('mywebsiteApp', []);
+var mywebsiteApp = angular.module('mywebsiteApp', ['ngRoute', 'ngResource']);
+
+//Routes
+mywebsiteApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/home.htm',
+      controller: 'homeController'
+    })
+    // .when('/', {
+    //   templateUrl: 'pages/contact.htm',
+    //   conroller: 'contactController'
+    // });
+});
+//controllers
+mywebsiteApp.controller('homeController', [$scope, function(){
+
+}]);
+
+//controllers
+// mywebsiteApp.controller('contactController', [$scope, function(){
+//
+// }]);
