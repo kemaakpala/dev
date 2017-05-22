0283b61616efe7a33bca26b81bdf27857f644b30
patrick akpala <patrick.a@thoughtbubble.com>
2017-05-22 10:20:17 +0100

    directives included, also updated the angular.js version been used but seem to have an issue with it. need fixing as nothing currently displays in the view

--- mywebsite/public/main.js
+++ mywebsite/public/main.js
@@ -1,23 +1,46 @@
 //Module
-var mywebsiteApp = angular.module('mywebsiteApp', ['ngRoute', 'ngResource']);
+var mywebsiteApp = angular.module('mywebsiteApp', ['ngRoute', 'ngResource', 'ngRequired', 'ui.bootstrap']);
 
 //Routes
 mywebsiteApp.config(function ($routeProvider) {
+
   $routeProvider
     .when('/', {
       templateUrl: 'pages/home.htm',
       controller: 'homeController'
     })
-    // .when('/', {
-    //   templateUrl: 'pages/contact.htm',
-    //   conroller: 'contactController'
-    // });
-});
-//controllers
-mywebsiteApp.controller('homeController', [$scope, function(){
 
-}]);
+});
+
+//services
+mywebsiteApp.service('contactService', function(){
+  this.contact = 'patrick';
+});
+
+
 //controllers
-// mywebsiteApp.controller('contactController', [$scope, function(){
-//
-// }]);
+mywebsiteApp.controller('homeController',
+  [
+    '$scope', 'contactService',
+    function($scope, contactService){
+      $scope.contact = contactService.contact;
+      $scope.$watch('contact', function(){
+        contactService.contact = $scope.contact;
+      });
+    }
+  ]
+);
+
+
+//directives
+mywebsiteApp.directive('contactForm', function(){
+  return {
+    restrict: 'EA',//EACM
+    templateUrl: 'directives/contact.htm',
+    replace: true //, //false does not remove custom tag from html
+    // scope: {
+    //   //isolated scope directive cannot directly affect what's going on in its parent page
+    //
+    // }
+  }
+});
