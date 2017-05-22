74737208a0e7e3ade7af80af046f7b6cad8c614d
Patrick Akpala <kemaakpala@gmail.com>
2017-05-22 12:10:13 +0100

    added bootstrap plugin
    and commented out services so as to fix contact directive issue

--- mywebsite/public/main.js
+++ mywebsite/public/main.js
@@ -1,5 +1,5 @@
 //Module
-var mywebsiteApp = angular.module('mywebsiteApp', ['ngRoute', 'ngResource', 'ngRequired', 'ui.bootstrap']);
+var mywebsiteApp = angular.module('mywebsiteApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
 
 //Routes
 mywebsiteApp.config(function ($routeProvider) {
@@ -13,9 +13,9 @@ mywebsiteApp.config(function ($routeProvider) {
 });
 
 //services
-mywebsiteApp.service('contactService', function(){
-  this.contact = 'patrick';
-});
+// mywebsiteApp.service('contactService', function(){
+//   this.contact = 'patrick';
+// });
 
 
 //controllers
@@ -23,10 +23,10 @@ mywebsiteApp.controller('homeController',
   [
     '$scope', 'contactService',
     function($scope, contactService){
-      $scope.contact = contactService.contact;
-      $scope.$watch('contact', function(){
-        contactService.contact = $scope.contact;
-      });
+      // $scope.contact = contactService.contact;
+      // $scope.$watch('contact', function(){
+      //   contactService.contact = $scope.contact;
+      // });
     }
   ]
 );
@@ -37,10 +37,10 @@ mywebsiteApp.directive('contactForm', function(){
   return {
     restrict: 'EA',//EACM
     templateUrl: 'directives/contact.htm',
-    replace: true //, //false does not remove custom tag from html
-    // scope: {
-    //   //isolated scope directive cannot directly affect what's going on in its parent page
-    //
-    // }
+    replace: true, //, //false does not remove custom tag from html
+    scope: {
+      //isolated scope directive cannot directly affect what's going on in its parent page
+
+    }
   }
 });
