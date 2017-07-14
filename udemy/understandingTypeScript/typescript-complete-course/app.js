20405b64dec73c55b07afa53b5c3918c3c213920
Patrick Akpala <kemaakpala@gmail.com>
2017-07-14 17:58:44 +0100

    typescript update and mywebsite update

--- udemy/understandingTypeScript/typescript-complete-course/app.js
+++ udemy/understandingTypeScript/typescript-complete-course/app.js
@@ -1,4 +1,14 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // implicitly implied types || do not assign types
 //string
 var myName = 'Patrick';
@@ -126,3 +136,192 @@ var myself = {
 };
 myself.bankAccount.deposit(3000);
 console.log(myself);
+//let & const
+console.log("Let & Console");
+var variable = "Test"; //block scope variable
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+var maxlevels = 100;
+console.log(maxlevels);
+//maxlevels = 99;
+function reset() {
+    var variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
+//Arrow Functions
+console.log("ARROW FUNCTIONS");
+var addNumbers = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(addNumbers(1, 2));
+var multiplyNumbers = function (number1, number2) { return number1 * number2; };
+console.log(multiplyNumbers(10, 3));
+var greet = function () {
+    console.log("Hello!");
+};
+greet();
+// const greetFriend = friend => console.log(friend);
+// greetFriend('nuno');
+// Default Parameters
+console.log("DEFAULT PARAMETERS");
+var countdown = function (start, end) {
+    if (start === void 0) { start = 10; }
+    if (end === void 0) { end = start - 5; }
+    while (start > end) {
+        start--;
+    }
+    console.log("Done", start);
+};
+countdown(10);
+//REST & SPREAD Operators
+console.log("REST & SPREAD: they allow you to work with arrays and list of values");
+var numbers = [1, 10, 99, -5];
+console.log('max List of numbers: ', Math.max(33, 99, 10, -3));
+console.log('max array using Spread operator: ', Math.max.apply(Math, numbers)); // spread operator converst array to list of values
+function makeArray(name) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    return args; // we return an array
+}
+console.log(makeArray("patrick", 1, 2, 6));
+//Destructuring
+console.log("DESTRUCTURING");
+var myHobbies = ["cooking", "sports"];
+// const hobby1 = myHobbies[0];
+// const hobby2 = myHobbies[1];
+var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
+console.log(hobby1, hobby2);
+var userData1 = { userName: "Patrick", age: 31 };
+var myName1 = userData1.userName, myAge1 = userData1.age;
+console.log(myName1, myAge1);
+//Template Literals
+console.log("TEMPLATE LITERALS");
+var userName2 = "Patrick";
+var greeting1 = "<strong>This is a heading!</strong>\nI'm " + userName2 + ".\nThis is cool!";
+console.log(greeting1);
+//OTHER ES6 FEATURES : SYMBOLS, ITERATORS AND GENERATORS
+//CLASS
+var Person = (function () {
+    function Person(name, username) {
+        this.username = username;
+        this.age = 27; // accessible from this class and inherited from this class. they can be initialised here not necessary to do it from  constructor
+        this.name = name;
+    }
+    //CLASS METHODS
+    Person.prototype.PrintAge = function () {
+        console.log(this.age);
+        this.setType("Old Guy"); //you have to use this when accessing methods or properties when inside the class
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
+    return Person;
+}());
+var person = new Person("Patrick", "patrick");
+console.log(person.name, person.username);
+person.PrintAge();
+// person.setType("Cool guy"); won't work with private method
+//Inheritance
+var Patrick = (function (_super) {
+    __extends(Patrick, _super);
+    //name: "Patrick";
+    function Patrick(username) {
+        var _this = _super.call(this, "Patrick", username) || this;
+        _this.age = 32; // this is available in the child class because it is protected
+        return _this;
+        //this.type this is private and only accessible from Person Class
+    }
+    return Patrick;
+}(Person));
+var patrick = new Patrick("patrick_1");
+console.log(patrick);
+//Getters & Setters
+var Plant = (function () {
+    function Plant() {
+        this._species = "Default";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = "Default";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+console.log("result:", plant.species);
+plant.species = "AB";
+console.log("result2:", plant.species);
+plant.species = "Green Plant";
+console.log("result3:", plant.species);
+//Static Properties & Methods
+var Helpers = (function () {
+    function Helpers() {
+    }
+    Helpers.calcCircumference = function (diameter) {
+        return this.PI * diameter;
+    };
+    Helpers.PI = 3.14; // this means you can instantiate this property without calling the class
+    return Helpers;
+}());
+console.log(2 * Helpers.PI);
+console.log('circumference:', Helpers.calcCircumference(8));
+//Abstract Classes: they cannot be instantiated, they are created to be inherited from
+var Project = (function () {
+    function Project() {
+        this.projectName = "Default";
+        this.budget = 1000;
+    }
+    Project.prototype.calcBudget = function () {
+        return this.budget * 2;
+    };
+    return Project;
+}());
+var ITProject = (function (_super) {
+    __extends(ITProject, _super);
+    function ITProject() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ITProject.prototype.changeName = function (name) {
+        this.projectName = name;
+    };
+    return ITProject;
+}(Project));
+var newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
+//constructors & singletons
+//private constructors
+var OnlyOne = (function () {
+    function OnlyOne(name) {
+        this.name = name;
+    }
+    OnlyOne.getInstance = function () {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    };
+    return OnlyOne;
+}());
+//let wrong = new OnlyOne('the Only One');
+var right = OnlyOne.getInstance();
+console.log(right.name);
+//right.name = 'Something else';
+console.log('-------------END of app.ts----------');
--- udemy/understandingTypeScript/typescript-complete-course/app.ts
+++ udemy/understandingTypeScript/typescript-complete-course/app.ts
@@ -157,3 +157,208 @@ let myself: { name: string, bankAccount: BankAccount , hobbies: string[]} = {
 myself.bankAccount.deposit(3000);
  
 console.log(myself);
+
+//let & const
+console.log("Let & Console");
+
+let variable = "Test"; //block scope variable
+console.log(variable);
+
+variable = "Another value";
+console.log(variable);
+const maxlevels = 100;
+console.log(maxlevels);
+//maxlevels = 99;
+
+function reset(){
+    let variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
+
+
+//Arrow Functions
+console.log("ARROW FUNCTIONS");
+const addNumbers = function(number1: number, number2: number): number{
+    return number1 + number2;
+};
+console.log(addNumbers(1 ,2));
+const multiplyNumbers = (number1: number, number2: number) =>  number1 * number2;
+console.log(multiplyNumbers(10,3));
+
+const greet = () => {
+    console.log("Hello!");
+    
+};
+greet();
+
+// const greetFriend = friend => console.log(friend);
+// greetFriend('nuno');
+
+// Default Parameters
+console.log("DEFAULT PARAMETERS");
+const countdown = (start: number = 10, end: number = start - 5): void => {
+    while(start > end){
+        start --;
+    }
+    console.log("Done", start);
+    
+};
+countdown(10);
+
+//REST & SPREAD Operators
+console.log("REST & SPREAD: they allow you to work with arrays and list of values");
+
+const numbers = [1,10,99,-5];
+console.log('max List of numbers: ', Math.max(33,99,10,-3));
+console.log('max array using Spread operator: ', Math.max(...numbers));// spread operator converst array to list of values
+
+function makeArray(name: string, ...args: number[]){ // we accept a list of values and convert them into an array
+    return args;// we return an array
+}
+console.log(makeArray("patrick",1,2,6));
+
+//Destructuring
+console.log("DESTRUCTURING");
+const myHobbies = ["cooking", "sports"];
+// const hobby1 = myHobbies[0];
+// const hobby2 = myHobbies[1];
+const[hobby1, hobby2] = myHobbies;
+console.log(hobby1, hobby2);
+
+const userData1 = { userName: "Patrick", age: 31 };
+const { userName:myName1, age:myAge1} = userData1;
+console.log(myName1, myAge1);
+
+//Template Literals
+console.log("TEMPLATE LITERALS");
+const userName2 = "Patrick";
+const greeting1 = `<strong>This is a heading!</strong>
+I'm ${userName2}.
+This is cool!`;
+console.log(greeting1);
+
+//OTHER ES6 FEATURES : SYMBOLS, ITERATORS AND GENERATORS
+
+//CLASS
+class Person {
+    name: string;
+    private type: string; // only accessible from this object
+    protected age:number = 27; // accessible from this class and inherited from this class. they can be initialised here not necessary to do it from  constructor
+
+    constructor(name:string, public username: string) {
+        this.name = name;
+    }
+
+    //CLASS METHODS
+    PrintAge() {
+        console.log(this.age);
+        this.setType("Old Guy"); //you have to use this when accessing methods or properties when inside the class
+    }
+
+   private setType(type: string){
+        this.type = type;
+        console.log(this.type);
+        
+    }
+}
+
+const person = new Person("Patrick", "patrick");
+console.log(person.name, person.username);
+
+person.PrintAge();
+// person.setType("Cool guy"); won't work with private method
+
+
+//Inheritance
+
+
+class Patrick extends Person {
+    //name: "Patrick";
+    constructor(username: string){
+        super("Patrick", username);
+        this.age = 32; // this is available in the child class because it is protected
+        //this.type this is private and only accessible from Person Class
+    }
+}
+
+const patrick = new Patrick("patrick_1");
+console.log(patrick);
+
+//Getters & Setters
+class Plant {
+    private _species: string = "Default";
+    get species() {
+        return this._species;
+    }
+
+    set species(value: string) {
+        if(value.length > 3){
+            this._species = value;
+        }else{
+            this._species = "Default";
+        }
+    }
+}
+
+let plant = new Plant();
+console.log("result:", plant.species);
+plant.species = "AB";
+console.log("result2:", plant.species);
+plant.species = "Green Plant"
+console.log("result3:", plant.species);
+
+//Static Properties & Methods
+class Helpers {
+    static PI: number = 3.14; // this means you can instantiate this property without calling the class
+    static calcCircumference (diameter: number):number {
+        return this.PI * diameter;
+    }
+}
+console.log(2 * Helpers.PI);
+console.log('circumference:',Helpers.calcCircumference(8));
+
+//Abstract Classes: they cannot be instantiated, they are created to be inherited from
+abstract class Project {
+    projectName: string = "Default";
+    budget: number = 1000;
+
+    abstract changeName(name: string): void; //means we need to create the logic for this changeName method in the childClass
+
+    calcBudget(){
+        return this.budget * 2;
+    }
+}
+
+class ITProject extends Project {
+    changeName(name: string) : void {
+        this.projectName = name;
+    }
+}
+
+let newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
+
+//constructors & singletons
+//private constructors
+class OnlyOne {
+    private static instance: OnlyOne;
+    private constructor (public readonly name: string) {}
+
+    static getInstance() {
+        if( !OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    }
+}
+
+//let wrong = new OnlyOne('the Only One');
+let right = OnlyOne.getInstance();
+console.log(right.name);
+//right.name = 'Something else';
+
+console.log('-------------END of app.ts----------');
