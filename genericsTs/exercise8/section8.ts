b8e802d96b1f4b1a300252f27e523f8dfe518eb1
Patrick Akpala <kemaakpala@gmail.com>
2017-11-28 17:31:22 +0000

    exercise8 solution done

--- genericsTs/exercise8/section8.ts
+++ genericsTs/exercise8/section8.ts
@@ -1,15 +1,47 @@
-class myMap <U extends string,T extends number | string>{
-    thisEmptyObject:{} = {};
-    setItem(key: string, item: T){
-        this.thisEmptyObject[key] = item; 
-    }
-    getItem(key: string){
-        return this.thisEmptyObject[key];
-    }
-    clear(){
+// class myMap <T>{
+//     private thisEmptyObject:{[key:string]: T} = {};
+//     setItem(key: string, item: T){
+//         this.thisEmptyObject[key] = item; 
+//     }
+//     getItem(key: string){
+//         return this.thisEmptyObject[key];
+//     }
+//     clear(){
+//         return this.thisEmptyObject = {};
+//     }
+//     printMap(){
+//         console.log(this.thisEmptyObject);
+//     }
+// }
 
-    }
-    printMap(){
+//recommended solution
 
+class MyMap <T> {
+    private map: {[key: string]: T} = {};
+
+    setItem (key: string, item: T){
+        this.map[key] = item;
+    }
+
+    getItem (key:string){
+        return this.map[key];
+    }
+
+    clear() {
+        this.map = {};
+    }
+
+    printMap () {
+        for (let key in this.map){
+            console.log(key, this.map);
+        }
     }
 }
+
+const numberMap = new MyMap <number> ();
+numberMap.setItem("apples", 10);
+numberMap.setItem("bananas", 2);
+console.log(numberMap.getItem("apples"));
+numberMap.printMap();
+numberMap.clear();
+numberMap.printMap();
