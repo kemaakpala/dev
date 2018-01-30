255eb47ca49d4418708bb497ea664e0a955fa36f
Patrick Akpala <kemaakpala@gmail.com>
2018-01-30 11:40:23 +0000

    chart.js completed, weather_list upgraded with Google Maps, style.css updated, google_maps component created

--- ReduxWeatherApp/src/components/chart.js
+++ ReduxWeatherApp/src/components/chart.js
@@ -11,7 +11,7 @@ export default (props) => {
                 <SparklinesLine color={props.color} />
                 <SparklinesReferenceLine type="avg" />
             </Sparklines>
-            <div>{average(props.data)}</div>
+            <div>{average(props.data)} {props.units}</div>
         </div>
     );
 }
