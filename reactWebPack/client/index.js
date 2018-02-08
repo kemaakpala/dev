3bf8d1095183dbac4fbf46f2e02cb74113e20ff1
Patrick Akpala <kemaakpala@gmail.com>
2018-02-08 13:28:04 +0000

    react, webPack, babel, starter pack

---
+++ reactWebPack/client/index.js
@@ -0,0 +1,9 @@
+/*
+  * ./client/index.js
+  * which is the webpack entry file
+*/
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDom.render(<App />, document.getElementById("root"))
